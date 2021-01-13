43e3b341e5ade29dee648f6399b9af525fcb822e
kentcamgit <77277076+kentcamgit@users.noreply.github.com>
2021-01-13 12:05:51 +0530

    new changes

---
+++ CamAttendance/Integration/API/API_Call_SampleCode/KentCam_ApiCall_SampleCode/KentCam_ApiCall_SampleCode/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KentCam_ApiCall_SampleCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Api_Methods OBJApiInterface = new Api_Methods();
+        private void btn_GetLog_Click(object sender, EventArgs e)
+        {
+            DateTime DT_from = dateTimePicker1.Value;
+            DateTime DT_To = dateTimePicker2.Value;
+            string from = dateTimePicker1.Text; string To = dateTimePicker2.Text;
+            string AccountId = "ACC-106";
+            string secureToken = "FUBQqq0kFPbXQzi";
+            string CompanyId = "0000";
+            string EmpId = "";
+            string PathtoCreateDataFile = @"D:\FileTodo.txt";
+            OBJApiInterface.GetTransactionLog(CompanyId, EmpId, AccountId, secureToken, DT_from, DT_To, PathtoCreateDataFile);
+
+        }
+
+        private void btn_PostData_Click(object sender, EventArgs e)
+        {
+            string FilePath = @"C:\KentFile\Transactionlog.txt";
+            string AccountId = "ACC-106";
+            string secureToken = "FUBQqq0kFPbXQzi";
+            var JsonData = OBJApiInterface.ReadDataFromTextFile(FilePath);
+
+            OBJApiInterface.HTTP_PostRequest(JsonData, AccountId, secureToken);
+        }
+    }
+}
