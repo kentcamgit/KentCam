787bb7ea3991045ffe22559755987763e63b40ce
kentcamgit <77277076+kentcamgit@users.noreply.github.com>
2021-01-14 11:55:10 +0530

    new changes

---
+++ CamAttendance/Integration/API/API_Call_SampleCode/API_Call_SampleCode/API_Functions.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.AccessControl;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace API_Call_SampleCode
+{
+    class API_Functions
+    {
+        private string _baseurl = "";
+        private DateTime _DT_from;
+        private DateTime _DT_To;
+        private string _AccountId = "";
+        private string _secureToken = "";
+        private string _PathtoCreateDataFile = "";
+        private string _JsonFilePathToAddEmp = "";
+
+        public API_Functions()
+        {
+            GetBaseUrl();
+        }
+
+        public string BaseUrl
+        {
+            get { return _baseurl; }
+            set { _baseurl = value; }
+        }
+        public DateTime DT_from
+        {
+            get { return _DT_from; }
+            set { _DT_from = value; }
+        }
+        public DateTime DT_To
+        {
+            get { return _DT_To; }
+            set { _DT_To = value; }
+        }
+        public string AccountId
+        {
+            get { return _AccountId; }
+            set { _AccountId = value; }
+        }
+        public string secureToken
+        {
+            get { return _secureToken; }
+            set { _secureToken = value; }
+        }
+        public string PathtoCreateDataFile
+        {
+            get { return _PathtoCreateDataFile; }
+            set { _PathtoCreateDataFile = value; }
+        }
+        public string JsonFilePathToAddEmp
+        {
+            get { return _JsonFilePathToAddEmp; }
+            set { _JsonFilePathToAddEmp = value; }
+        }
+
+        public void AddEmployeeExample(dynamic jsonData, string base64EncodedStr)
+        {
+            try
+            {
+
+                HttpClientHandler handler = new HttpClientHandler();
+                HttpClient client = new HttpClient(handler);
+
+                string messageUri = BaseUrl + "employee";
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(messageUri);
+                var BodyPara = base64EncodedStr;
+                request.Headers.Add("token", base64EncodedStr);
+                request.ContentType = "application/json";
+                request.Method = "Post";
+
+                request.Accept = "application/json";
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                {
+                    streamWriter.Write(JsonConvert.DeserializeObject(jsonData));
+                }
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                Stream responseStream = response.GetResponseStream();
+
+                string jsonString = null;
+
+                using (StreamReader reader = new StreamReader(responseStream))
+                {
+                    jsonString = reader.ReadToEnd();
+                    Console.WriteLine();
+                    reader.Close();
+                }
+            }
+            catch (Exception ee) { }
+        }
+
+        public void GetTransactionLogExample(string Base64EncodedToken)
+        {
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                string lastEvaluatedKey = ""; string Size50 = "&size=50";
+                HttpClientHandler handler = new HttpClientHandler();
+                HttpClient client = new HttpClient(handler);
+                string content = string.Empty;
+
+                string DateFrom = GetTimeInMiliSec(DT_from);
+                string date_To = GetTimeInMiliSec(DT_To);
+            Found:
+
+                string messageUri = BaseUrl + "transactions?from=" + DateFrom + "&to=" + date_To + lastEvaluatedKey + Size50;
+
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(messageUri);
+                var BodyPara = Base64EncodedToken;
+
+                request.Headers.Add("token", Base64EncodedToken);
+                request.ContentType = "application/json";
+                request.Method = "Get";
+
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+                Stream responseStream = response.GetResponseStream();
+                string jsonString = null;
+
+                using (StreamReader reader = new StreamReader(responseStream))
+                {
+                    jsonString = reader.ReadToEnd();
+                    reader.Close();
+                }
+
+                dynamic dynObj = JsonConvert.DeserializeObject(jsonString);
+
+                foreach (var Root in dynObj.Root)
+                {
+                    foreach (var data in Root)
+                    {
+                        foreach (JProperty keyValue in data)
+                        {
+                            string KeyName = keyValue.Name.ToString();
+                            var KeyVal = keyValue.Value;
+                            if (KeyName == "list")
+                            {
+                                foreach (var data1 in KeyVal)
+                                {
+                                    sb.Append("{");
+                                    foreach (JProperty keyValue2 in data1)
+                                    {
+                                        string KeyName2 = keyValue2.Name.ToString();
+                                        var KeyVal2 = keyValue2.Value;
+                                        sb.Append(KeyName2 + ":" + KeyVal2 + ",");
+                                    }
+                                    sb.Append("},\r\n");
+                                }
+                            }
+                            if (KeyName == "lastEvaluatedKey")
+                            {
+                                if (KeyVal.ToString() != "")
+                                {
+                                    lastEvaluatedKey = "";
+                                    lastEvaluatedKey = "&lastEvaluatedKey=" + KeyVal;
+                                    goto Found;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { }
+            finally
+            {
+                CreateTextFile(sb);
+            }
+        }
+        public string EncryptString()
+        {
+            long currentTimeMillis = (long)DateTime.Now.ToUniversalTime().Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds;
+
+            string base64Decoded = secureToken + "_" + currentTimeMillis + "_" + AccountId;
+            string base64Encoded;
+            byte[] data = System.Text.ASCIIEncoding.ASCII.GetBytes(base64Decoded);
+            base64Encoded = System.Convert.ToBase64String(data);
+            return base64Encoded;
+        }
+        public string ReadDataFromTextFile()
+        {
+            string JsonData = string.Empty;
+            if (File.Exists(JsonFilePathToAddEmp))
+            {
+                JsonData = File.ReadAllText(JsonFilePathToAddEmp);
+            }
+            return JsonData;
+        }
+        public string GetTimeInMiliSec(DateTime Time)
+        {
+            long TimeInMiliSec = (long)Time.ToUniversalTime().Subtract(new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc)).TotalMilliseconds; ;
+            return TimeInMiliSec.ToString();
+        }
+        public void CreateTextFile(StringBuilder sb)
+        {
+            if (!File.Exists(PathtoCreateDataFile))
+            {
+                using (StreamWriter sw = File.CreateText(PathtoCreateDataFile))
+                {
+                    sw.WriteLine(sb.ToString());
+                }
+            }
+        }
+        public void GetBaseUrl()
+        {
+            BaseUrl = File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "url.txt"));
+        }
+    }
+}
---
+++ CamAttendance/Integration/API/API_Call_SampleCode/API_Call_SampleCode/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace API_Call_SampleCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btn_GetTransactionLog = new System.Windows.Forms.Button();
+            this.lbl_DateFrom = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btn_AddEmployee = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.lbl_DateFrom);
+            this.groupBox1.Controls.Add(this.btn_GetTransactionLog);
+            this.groupBox1.Location = new System.Drawing.Point(25, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(363, 247);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Get_Transaction_Data";
+            // 
+            // btn_GetTransactionLog
+            // 
+            this.btn_GetTransactionLog.Location = new System.Drawing.Point(113, 157);
+            this.btn_GetTransactionLog.Name = "btn_GetTransactionLog";
+            this.btn_GetTransactionLog.Size = new System.Drawing.Size(144, 55);
+            this.btn_GetTransactionLog.TabIndex = 0;
+            this.btn_GetTransactionLog.Text = "Get Transaction Log";
+            this.btn_GetTransactionLog.UseVisualStyleBackColor = true;
+            this.btn_GetTransactionLog.Click += new System.EventHandler(this.btn_GetTransactionLog_Click);
+            // 
+            // lbl_DateFrom
+            // 
+            this.lbl_DateFrom.AutoSize = true;
+            this.lbl_DateFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_DateFrom.Location = new System.Drawing.Point(24, 69);
+            this.lbl_DateFrom.Name = "lbl_DateFrom";
+            this.lbl_DateFrom.Size = new System.Drawing.Size(65, 15);
+            this.lbl_DateFrom.TabIndex = 1;
+            this.lbl_DateFrom.Text = "Date From";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(24, 108);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Date To";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(113, 64);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Location = new System.Drawing.Point(113, 103);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker2.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btn_AddEmployee);
+            this.groupBox2.Location = new System.Drawing.Point(408, 42);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(323, 250);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Add_Employee";
+            // 
+            // btn_AddEmployee
+            // 
+            this.btn_AddEmployee.Location = new System.Drawing.Point(113, 147);
+            this.btn_AddEmployee.Name = "btn_AddEmployee";
+            this.btn_AddEmployee.Size = new System.Drawing.Size(144, 55);
+            this.btn_AddEmployee.TabIndex = 1;
+            this.btn_AddEmployee.Text = "Add Employee";
+            this.btn_AddEmployee.UseVisualStyleBackColor = true;
+            this.btn_AddEmployee.Click += new System.EventHandler(this.btn_AddEmployee_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(781, 345);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbl_DateFrom;
+        private System.Windows.Forms.Button btn_GetTransactionLog;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btn_AddEmployee;
+    }
+}
+
